326e272ee16ba9d35ad60619fed4c5a570c2c73b
LeeGee64 <writerlelandgill@gmail.com>
2022-08-27 12:06:42 -0400

    layout styling

--- components/layout.js
+++ components/layout.js
@@ -63,50 +63,52 @@ export default function Layout({ children, home }) {
       </header>
     
 
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
       
       
       <footer id="footer" className={styles.footer}>
-        <a className={utilStyles.logo} href="https://twitter.com/LeeGee64">
-        <Image
-              priority
-              src="/Images/logos/2021-Twitter-logo-blue.png"
-              height= {30}
-              width= {30}
-              className={utilStyles.logo}
-              alt="Twitter"
-            />
-          </a>
-        <a className={utilStyles.logo} href="https://www.instagram.com/who_is_lee888/">
+        <div className={styles.linkList}>
+          <a className={utilStyles.logo} href="https://twitter.com/LeeGee64">
           <Image
-              priority
-              src="/Images/logos/Instagram_Glyph_Gradient.png"
-              height= {30}
-              width= {30}
-              className={utilStyles.logo}
-              alt="Instagram"
-            />
-          </a>
-        <a className={utilStyles.logo} href="https://github.com/LeeGee64">
-          <Image
-              priority
-              src="/Images/logos/GitHub-Mark-64px.png"
-              height= {30}
-              width= {30}
-              className={utilStyles.logo}
-              alt="GitHub"
-            />
-          </a>
-        <a className={utilStyles.logo} href="https://www.linkedin.com/in/leland-gill">
-          <Image
-              priority
-              src="/Images/logos/In-Blue-34.png"
-              height= {30}
-              width= {30}
-              className={utilStyles.logo}
-              alt="LinkedIn"
-            />
-          </a>
+                priority
+                src="/Images/logos/2021-Twitter-logo-blue.png"
+                height= {30}
+                width= {30}
+                className={utilStyles.logo}
+                alt="Twitter"
+              />
+            </a>
+          <a className={utilStyles.logo} href="https://www.instagram.com/who_is_lee888/">
+            <Image
+                priority
+                src="/Images/logos/Instagram_Glyph_Gradient.png"
+                height= {30}
+                width= {30}
+                className={utilStyles.logo}
+                alt="Instagram"
+              />
+            </a>
+          <a className={utilStyles.logo} href="https://github.com/LeeGee64">
+            <Image
+                priority
+                src="/Images/logos/GitHub-Mark-64px.png"
+                height= {30}
+                width= {30}
+                className={utilStyles.logo}
+                alt="GitHub"
+              />
+            </a>
+          <a className={utilStyles.logo} href="https://www.linkedin.com/in/leland-gill">
+            <Image
+                priority
+                src="/Images/logos/In-Blue-34.png"
+                height= {30}
+                width= {30}
+                className={utilStyles.logo}
+                alt="LinkedIn"
+              />
+            </a>
+          </div>
       </footer>
     </div>
   );
