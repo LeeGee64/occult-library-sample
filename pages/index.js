e0f2ab1f7a5b2085047b81fd1647e0b97aba398b
LeeGee64 <writerlelandgill@gmail.com>
2022-08-29 12:18:26 -0400

    index styling

--- pages/index.js
+++ pages/index.js
@@ -24,38 +24,16 @@ export default function Home({ bookList }) {
         <h2>
           Your quest for esoteric knowledge begins here.
         </h2>
+        <div className= {utilStyles.centerRowBlock}>
+          <div className= {utilStyles.columnBlock}>
+            <p>Don't know how to start? Click the button below for a random selection.</p>
+            <button className= {utilStyles.button} type= "button" onClick={ () => getRandom({ bookList })}>
+              Random Book 
+            </button>
+            <ul className= {utilStyles.randomDisplay}>{ randomList.map(random => {return (<li id={random.isbn}><BookCard book= {random}></BookCard> </li>)}) } </ul>
+          </div>
+        </div>
 
-        <p>
-          This app is designed to ease research into Spiritualism, Magick, and the general Occult. 
-        </p>
-
-        <p>
-          Learn more about this app{' '}
-          <Link href= "/about">
-            <a>here.</a>
-          </Link>
-        </p>
-
-        <p>
-          Browse the full collection{' '}
-          <Link href= "/directory">
-            <a>here.</a>
-          </Link>
-        </p>   
-
-        <p>
-          Search for specific texts{' '}
-          <Link href= "/booksearch">
-            <a>here.</a>
-          </Link>
-        </p>            
-        
-        <p>Don't know how to start? Click the button below for a random selection</p>
-        <button type= "button" onClick={ () => getRandom({ bookList })}>
-          Random Book 
-        </button>
-        <ul>{ randomList.map(random => {return (<li id={random.isbn}><BookCard book= {random}></BookCard> </li>)}) } </ul>
-      
     </Layout> 
 )}
 
