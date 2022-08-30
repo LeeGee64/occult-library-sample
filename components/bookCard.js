71e0a2f25feae12d474186555e42f0c073a873c5
LeeGee64 <writerlelandgill@gmail.com>
2022-08-30 13:01:41 -0400

    card styling

--- components/bookCard.js
+++ components/bookCard.js
@@ -18,11 +18,12 @@ export default function BookCard ( { book } ) {
           <div className= { cardStyle.bookDescription }>
             <a href= { book.read_link }><h2 className={ cardStyle.bookTitle }> { book.title } </h2></a>
             <h3 className={ cardStyle.bookAuthor }> { book.author } </h3>
+            <hr className= { cardStyle.divider }></hr>
             { book.additional_authors ? <p> { book.additional_authors } </p> : null}
-            <p>ISBN: { book.isbn ? book.isbn : "N/A"} </p>
+            <p> <span className = { cardStyle.strong }>ISBN:</span> { book.isbn ? book.isbn : "N/A"} </p>
             <div className= { cardStyle.additionalDetails}>
-                <p> Year: {book.year_published ? book.year_published : "N/A"} </p>
-                <p> Pages: { book.number_of_pages ? book.number_of_pages : "N/A"} </p>
+                <p> <span className = { cardStyle.strong }>Year:</span> {book.year_published ? book.year_published : "N/A"} </p>
+                <p> <span className = { cardStyle.strong }>Pages:</span> { book.number_of_pages ? book.number_of_pages : "N/A"} </p>
             </div>
           </div>
           </div>
