08050d5447c8169c55d1a40b80c374686ff465dd
LeeGee64 <writerlelandgill@gmail.com>
2022-08-31 17:26:46 -0400

    installed useMeasure, adding animation

--- components/bookCard.js
+++ components/bookCard.js
@@ -2,12 +2,13 @@ import Image from 'next/image';
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import React, { useState } from 'react';
 import cardStyle from './bookCard.module.css';
+import useMeasure from 'react-use-measure';
 
 
 export default function BookCard ( { book } ) {
 
   const [toggle, setToggle] = React.useState(false);
-  // const [ref, bounds] = useMeasure(); 
+  const [ref, { height }] = useMeasure(); 
 
     return (
      
@@ -29,11 +30,12 @@ export default function BookCard ( { book } ) {
           </button>
           </div>
           
-          {toggle ? (<motion.div 
-                      key={toggle}
-                      initial={{height: 0}}
-                      animate={{ height: "auto"}}
-                      className= { cardStyle.bookDescription }>   
+          <motion.div animate={{ height }}>
+          <div ref= {ref}>
+          {toggle ? (
+            <AnimatePresence>
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1}}>
+            <div className= { cardStyle.bookDescription }>   
             <a href= { book.read_link }><h2 className={ cardStyle.bookTitle }> { book.title } </h2></a>
             <h3 className={ cardStyle.bookAuthor }> { book.author } </h3>
             <hr className= { cardStyle.divider }></hr>
@@ -43,9 +45,14 @@ export default function BookCard ( { book } ) {
                 <p> <span className = { cardStyle.strong }>Year:</span> {book.year_published ? book.year_published : "N/A"} </p>
                 <p> <span className = { cardStyle.strong }>Pages:</span> { book.number_of_pages ? book.number_of_pages : "N/A"} </p>
             </div>
-          </motion.div>) : null}
-        
+            </div>
+            </motion.div>
+            </AnimatePresence>
+          ) : null}
         </div>
+        </motion.div>
+        </div>
+        
         </MotionConfig>
       </div>
     )
