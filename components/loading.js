4227a5fae9c90b732df4b3d1b6617c0844e1e367
LeeGee64 <writerlelandgill@gmail.com>
2022-09-01 14:57:22 -0400

    loading animation

--- components/loading.js
+++ components/loading.js
@@ -1,15 +1,19 @@
 import loadStyles from './loading.module.css';
 import Image from 'next/image';
+import { motion } from 'framer-motion'; 
 
 export default function Loading (props){
     return (
         <div className={props.loading ? loadStyles.bodyLoading : loadStyles.none}>
-            <h1 className={loadStyles.loadingTitle}>LOADING</h1>
-            <div className={loadStyles.loadingImage}><Image
+            <h1 className={loadStyles.loadingTitle}>SUMMONING</h1>
+            <motion.div 
+            animate= {{rotate: 360}}
+            transition= {{duration: 1, repeat: Infinity, repeatDelay: 1 }}
+            className={loadStyles.loadingImage}><Image
                 src= '/Images/loading_images/sacredGeo.png' 
                 height={100}
                 width={100}
-            /></div>      
+            /></motion.div>      
         </div>
     );
 }
